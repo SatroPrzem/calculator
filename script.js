aa896a5fd2bda0392197dfc50af3af1618d1d4bd
Korean Przemek <przemyslaw.satro@gmail.com>
2022-02-07 21:28:48 +0100

    finished calculator (for now ;) )

--- script.js
+++ script.js
@@ -10,21 +10,83 @@ let actualNumber = '';
 let previousNumber = '';
 let operation = undefined;
 
+const calculate = () => {
+    let calculation = '';
+    if(!previousNumber || !actualNumber) return;
+    const actual = parseFloat(actualNumber);
+    const prev = parseFloat(previousNumber);
+    if(isNaN(actual)  || isNaN(prev)) return;
+
+    switch(operation) {
+        case '+':
+            calculation = prev + actual;
+            break;
+        case "-":
+            calculation = prev - actual;
+            break;
+        case "×":
+            calculation = prev * actual;
+            break;
+        case "÷" :
+            if(actual === 0){
+                clearCalculator();
+                return;
+            } 
+            calculation = prev / actual;
+            break;
+        case "√" : 
+            calculation = Math.pow(prev, 1/actual);
+            break; 
+        case "^" : 
+            calculation = Math.pow(prev, actual);
+            break;
+        case "%" : 
+            calculation = prev / 100 * actual;
+            break;
+        case "log" :
+            if(actual === 1){
+                clearCalculator();
+                return;
+            }
+            calculation = Math.log(prev) / Math.log(actual); 
+            break;
+        default:return;
+    }
+
+    actualNumber = calculation.toString();
+    operation = undefined;
+    previousNumber = '';
+}
+
 const clearCalculator = () => {
     actualNumber = '';
     previousNumber = '';
     operation = undefined;
 };
-const chocenOperation = (operator) => {
+const chooseOperation = (operator) => {
     if(actualNumber === '') return;
+    if (previousResult !== '') {
+        const prev = previousResult.innerText;
+        if(actualNumber.toString() === "0" && prev[prev.length -1 ] === "÷") {
+            clearCalculator();
+            return;
+        }
+        if(actualNumber.toString() === "1" && prev[prev.length -1 ] === "g") {
+            clearCalculator();
+            return;
+        }
+        calculate();
+    }
     operation = operator;
-    previousNumber = actualNumber;
+    previousNumber = actualNumber ;
     actualNumber = '';
 }
 
 const updateResult = () => {
     actualResult.innerText = actualNumber;
-    previousResult.innerText = previousNumber;
+    if (operation != null) previousResult.innerHTML = previousNumber  + operation;
+    else previousResult.innerText = ''
+
 }
 
 const addNumber = (num) => {
@@ -46,13 +108,6 @@ const deleteNumber = () => {
     };
 };
 
-
-
-
-
-
-
-
 numbers.forEach((num) => {
     num.addEventListener('click', () => {
         addNumber(num.innerText);
@@ -61,9 +116,10 @@ numbers.forEach((num) => {
 });
 
 operators.forEach((operator) => {
-    operator.addEventListener('click', () =>{
-        console.log('siemaneczko');
-    });
+    operator.addEventListener('click', () => {
+        chooseOperation(operator.innerText);
+        updateResult();
+    })
 });
 
 deleteBtn.addEventListener('click', () => {
@@ -75,3 +131,15 @@ clearBtn.addEventListener('click', () => {
     clearCalculator();
     updateResult();
 });
+
+operators.forEach((op) => {
+    op.addEventListener('click', () => {
+        chooseOperation(op.innerText);
+        updateResult();
+    })
+})
+
+equalBtn.addEventListener('click', () => {
+    calculate();
+    updateResult();
+})
